396a3ce6aafb50c68dab12fa754c001bcfab96db
KhaledTETAH <fk_tetah@esi.dz>
2023-03-03 03:25:32 +0100

    all good

--- tickets_project/urls.py
+++ tickets_project/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tickets_app/v1/', include('apps.tickets_app.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
