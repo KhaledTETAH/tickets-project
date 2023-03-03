4e2445e7b5bfd8dc629bc09f02ff04774c5b425b
KhaledTETAH <fk_tetah@esi.dz>
2023-03-03 01:53:55 +0100

    all good

--- tickets_project/urls.py
+++ tickets_project/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
 
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
